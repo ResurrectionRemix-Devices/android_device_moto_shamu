4d92e6b31a82ab272bf2e9a40343316bb175a0e6
Pranav Vashi <neobuddy89@gmail.com>
2016-08-16 15:01:53 +0530

    shamu: Add blur support
    Change-Id: Iec44aaecf000767a244103458a6057977f6371c7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- device.mk
+++ device.mk
@@ -47,6 +47,9 @@ PRODUCT_COPY_FILES += \
     device/moto/shamu/audio_platform_info.xml:system/etc/audio_platform_info.xml
 
 PRODUCT_COPY_FILES += \
+    device/moto/shamu/blur/libuiblur.so:system/vendor/lib/libuiblur.so
+
+PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
